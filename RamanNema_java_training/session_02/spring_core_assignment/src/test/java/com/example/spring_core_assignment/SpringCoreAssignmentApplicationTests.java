58db59803c3f23a13620b7d15a4ad19f169d6024
Raman Nema <ramannema@Ramans-MacBook-Air.local>
2026-04-11 11:03:18 +0530

    feat(java spring core): Initial Spring Boot setup with Maven and package structure (controller, service, repository, etc).

---
+++ RamanNema_java_training/session_02/spring_core_assignment/src/test/java/com/example/spring_core_assignment/SpringCoreAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_core_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
