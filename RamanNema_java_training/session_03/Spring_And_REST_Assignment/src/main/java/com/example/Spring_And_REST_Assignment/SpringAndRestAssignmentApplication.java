dc34da26635fd73b0f91005fda434b3ed301024a
Raman Nema <ramannema@Ramans-MacBook-Air.local>
2026-04-15 09:50:29 +0530

    feat(java rest api): Initial spring boot project setup.

---
+++ RamanNema_java_training/session_03/Spring_And_REST_Assignment/src/main/java/com/example/Spring_And_REST_Assignment/SpringAndRestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_And_REST_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndRestAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndRestAssignmentApplication.class, args);
+	}
+
+}
---
+++ RamanNema_java_training/session_03/Spring_And_REST_Assignment/src/test/java/com/example/Spring_And_REST_Assignment/SpringAndRestAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_And_REST_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndRestAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
