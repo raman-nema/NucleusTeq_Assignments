d93b37b1e6f1736a36e85b467038ccb5620a1c8e
Raman Nema <ramannema@Ramans-MacBook-Air.local>
2026-04-17 18:23:39 +0530

    feat(java todo app): Created TodoResponseDTO to structure API responses and avoid exposing internal entity directly.

---
+++ RamanNema_java_training/session_04/todo_application/src/main/java/com/example/todo_application/dto/TodoResponseDTO.java
@@ -0,0 +1,55 @@
+package com.example.todo_application.dto;
+
+import com.example.todo_application.entity.Status;
+import java.time.LocalDateTime;
+
+// DTO for response
+public class TodoResponseDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private Status status;
+    private LocalDateTime createdAt;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
